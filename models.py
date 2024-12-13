6b8de9bf512ddcf553b96d623523b212fd148620
Samuel Morgan <samuel.edmund.morgan@icloud.com>
2024-12-13 16:25:05 +0200

    Init commit

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class DBAuthor(Base):
+    __tablename__ = 'author'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True, index=True)
+    bio = Column(String(512), index=True)
+
+
+class DBBook(Base):
+    __tablename__ = 'book'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), index=True)
+    summary = Column(String(512), index=True)
+    publication_date = Column(Date, index=True)
+    author_id = Column(Integer, ForeignKey('author.id'))
+    author = relationship(DBAuthor)
